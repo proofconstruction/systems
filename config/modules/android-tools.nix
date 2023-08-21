01bfdd4bc9e7e112814a32f96906067ed46898db
Alexander Groleau <source@proof.construction>
2023-08-21 18:21:03 -0500

    add v4l2loopback, enabling android phone webcam

--- config/modules/android-tools.nix
+++ config/modules/android-tools.nix
@@ -7,8 +7,23 @@
   options.mine.android-tools.enable = lib.mkEnableOption "adb";
 
   config = lib.mkIf config.mine.android-tools.enable {
+
+    # allow using android device camera as webcam
+    boot = {
+      kernelModules = [
+        "v4l2loopback"
+        "snd-aloop"
+      ];
+      extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+
+      extraModprobeConfig = ''
+        options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+      '';
+    };
+
     home-manager.users.${config.mine.user.name}.home.packages = with pkgs; [
       android-tools
+      (scrcpy.override { ffmpeg = ffmpeg_6.override { withV4l2 = true; }; })
     ];
   };
 }
