6c7e1b87fec7cc934139d85464688ddd86e7b42b
Alexander Groleau <source@proof.construction>
2023-08-22 18:56:56 -0500

    enable nvidia passthough to podman containers

--- config/modules/podman.nix
+++ config/modules/podman.nix
@@ -7,7 +7,16 @@
 {
   options.mine.podman.enable = lib.mkEnableOption "podman";
 
-  config = lib.mkIf config.mine.podman.enable {
-    virtualisation.podman.enable = true;
-  };
+  config = lib.mkMerge [
+    (lib.mkIf config.mine.podman.enable {
+      virtualisation.podman = {
+        enable = true;
+        dockerCompat = true;
+      };
+    })
+
+    (lib.mkIf config.mine.nvidia.enable {
+      virtualisation.podman.enableNvidia = true;
+    })
+  ];
 }
