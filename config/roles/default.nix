30773bbd35364611f47bd0240995c46ca00b3ef6
alex <source@proof.construction>
2024-08-13 19:23:54 -0400

    don't manually import roles

--- config/roles/default.nix
+++ config/roles/default.nix
@@ -1,13 +1,38 @@
+# Stolen from https://github.com/infinisil/system/blob/master/config/new-modules/default.nix; thanks Silvan!
+
+{ config
+, lib
+, ...
+}:
+
+# {
+#   imports = config.mine.lib.validFiles ./.;
+# }
+with lib;
+let
+  # Recursively constructs an attrset of a given directory
+  getDir = dir: mapAttrs
+    (file: type:
+      if type == "directory"
+      then getDir "${dir}/${file}"
+      else type)
+    (builtins.readDir dir);
+
+  # Collects all files of a directory as a list of path strings
+  files = dir: collect isString
+    (mapAttrsRecursive
+      (pathstr: type: concatStringsSep "/" pathstr)
+      (getDir dir));
+
+  filteredFiles = files:
+    filter
+      # Filters out files that don't end with .nix, ignoring this file.
+      (file: hasSuffix ".nix" file && file != "default.nix")
+      files;
+
+  # Produces a list of absolute paths to non-`default.nix` nix files in `dir`.
+  validFiles = dir: map (file: ./. + "/${file}") (filteredFiles (files dir));
+in
 {
-  imports = [
-    ./common.nix
-    ./binary-cache.nix
-    ./database.nix
-    ./git-server.nix
-    ./laptop.nix
-    ./router.nix
-    ./server.nix
-    ./vm-host.nix
-    ./workstation.nix
-  ];
+  imports = validFiles ./.;
 }
